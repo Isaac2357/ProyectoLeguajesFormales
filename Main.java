f5d766a5267356077a9739f81fe072bca0e8d2e3
joaquin100 <is708601@iteso.mx>
2019-05-04 16:01:25 -0500

    M1 implementation ER to postfix notation

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main{
     
     public static void main(String[] args) {
+        System.out.println("sssss");
         Interface myInterface = new Interface();
 		myInterface.setVisible(true);
     }
     
 }
+
